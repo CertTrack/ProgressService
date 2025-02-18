aeb29786a6efd2c11c7a809bef99149e510ee1bf
Dmytro <150928240+Dmytro-Trofimov@users.noreply.github.com>
2025-02-18 22:47:11 +0100

    refactor

--- src/main/java/com/certTrack/ProgressTrackingService/Controllers/ProgressController.java
+++ src/main/java/com/certTrack/ProgressTrackingService/Controllers/ProgressController.java
@@ -3,6 +3,7 @@ package com.certTrack.ProgressTrackingService.Controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,45 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.certTrack.ProgressTrackingService.DTO.ResponseMessage;
 import com.certTrack.ProgressTrackingService.Entity.Progress;
+import com.certTrack.ProgressTrackingService.Security.UserPrincipal;
 import com.certTrack.ProgressTrackingService.Service.ProgressService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/progress")
+@RequiredArgsConstructor
 public class ProgressController {
 
     private final ProgressService progressService;
 
-    public ProgressController(ProgressService progressService) {
-        this.progressService = progressService;
-    }
-
-    
-    
-    
     @PutMapping("/update")
     public ResponseEntity<ResponseMessage> updateProgress(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal UserPrincipal user,
             @RequestParam Long courseId,
             @RequestParam int progressPercentage){
-        progressService.updateProgress(userId, courseId, progressPercentage);
+        progressService.updateProgress(user.getUserId(), courseId, progressPercentage);
         return ResponseEntity.ok(new ResponseMessage("Progress updated successfully."));
     }
 
     @GetMapping("/get")
     public Progress getProgress(
-            @RequestParam Long userId,
+    		@AuthenticationPrincipal UserPrincipal user,
             @RequestParam Long courseId) {
-        return progressService.getProgressByUserIdAndCourseId(userId, courseId);
+        return progressService.getProgressByUserIdAndCourseId(user.getUserId(), courseId);
     }
     
     @GetMapping("/all")
     public List<Progress> getAllUserProgress(
-            @RequestParam Long userId) {
-        return progressService.getProgressByUserId(userId);
+    		@AuthenticationPrincipal UserPrincipal user) {
+        return progressService.getProgressByUserId(user.getUserId());
     }
-    
-
-    
     
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseMessage> clearProgressByUserIdAndCourseId(
